95cb3c1502e3683dd3f852926b0a3d8327ff366a
Iiiiiit <weiyongtao@weiyongtaodeMacBook-Pro.local>
2015-12-03 13:47:58 +0800

    重构完成

--- js/app.js
+++ js/app.js
@@ -1,8 +1,42 @@
 var bookstore = angular.module('bookstore', ['ui.router']);
 
-bookstore.run(function($rootScope, $state, $stateParams) {
+bookstore.run(function($rootScope, $state, $stateParams, getJson) {
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
+    var promise = getJson.books();
+    promise.then(function(date) {
+        $rootScope.books = date;
+    }, function() {
+        console.log("失败");
+    })
+});
+
+// filter
+bookstore.filter('searchBook', function() {
+    return function(bookList, keyword) {
+        var reg = new RegExp(keyword);
+        var output = [];
+        for (var i = 0; i < bookList.length; i++) {
+            var book = bookList[i];
+            if (reg.test(book.name)) {
+                output.push(book);
+                continue;
+            }
+            if (reg.test(book.author)) {
+                output.push(book);
+                continue;
+            }
+            if (reg.test(book.category)) {
+                output.push(book);
+                continue;
+            }
+            if (reg.test(book.publish)) {
+                output.push(book);
+                continue;
+            }
+        }
+        return output;
+    }
 });
 
 // 配置路由
@@ -13,6 +47,9 @@ bookstore.config(function($stateProvider, $urlRouterProvider) {
             url: '/index',
             views: {
                 '': {
+                    templateUrl: 'templates/booksContainer.html'
+                },
+                'bookList@index': {
                     templateUrl: 'templates/deflist.html'
                 }
             }
@@ -25,7 +62,7 @@ bookstore.config(function($stateProvider, $urlRouterProvider) {
                 }
             }
         })
-        .state('add',{
+        .state('add', {
             url: '/addBook',
             views: {
                 '': {
@@ -33,10 +70,10 @@ bookstore.config(function($stateProvider, $urlRouterProvider) {
                 }
             }
         })
-        .state('edit',{
+        .state('edit', {
             url: '/editBookInfo/{bookID:[0-9]{1,4}}',
             views: {
-                '':{
+                '': {
                     templateUrl: 'templates/form.html'
                 }
             }
--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,83 @@
-bookstore.controller('deflistCtrl', function($rootScope, getJson) {
-    var promise = getJson.books();
-    promise.then(function(date) {
-        $rootScope.books = date;
-    }, function() {
-        console.log("失败");
-    })
+bookstore.controller('searchCtrl', function($scope) {
+    $scope.keyword = '';
+    $scope.title = '重磅推荐';
+    $scope.keywordChange = function() {
+        if ($scope.keyword !== '') {
+            $scope.title = '搜索结果';
+        } else {
+            $scope.title = '重磅推荐';
+        }
+    }
+})
+
+bookstore.controller('listCtrl', function($scope, $rootScope, getJson) {
+    if ($rootScope.books) {
+
+    } else {
+        var promise = getJson.books();
+        promise.then(function(date) {
+            $rootScope.books = date;
+        }, function() {
+            console.log("失败");
+        })
+    }
+    $scope.del = function(bookID) {
+        for (var i = 0; i < $rootScope.books.length; i++) {
+            if (bookID == $rootScope.books[i].bookID) {
+                $rootScope.books.splice(i, 1);
+                break;
+            }
+        }
+    }
 });
 
-bookstore.controller('bookDetail', function($scope, $rootScope, $stateParams, getJson) {
+bookstore.controller('formCtrl', function($scope, $rootScope, $state, $stateParams, $http) {
+    var bookList = $rootScope.books;
+    var bookIndex = 0;
+    $http({
+        method: 'GET',
+        url: 'publish.json'
+    }).then(function successCallback(response) {
+        $scope.publishs = response.data.publishs;
+        console.log(response.data.publishs);
+    }, function errorCallback(response) {
+        console.log('get publish.json error!');
+    });
+    $scope.book = {};
+    if ($stateParams.bookID) {
+        var bookId = $stateParams.bookID;
+        for (bookIndex; bookIndex < bookList.length; bookIndex++) {
+            if (bookId == bookList[bookIndex].bookID) {
+                $scope.book = cloneObj(bookList[bookIndex]);
+                break;
+            }
+        }
+        $scope.submit = function() {
+            bookList[bookIndex] = $scope.book;
+            $state.go('index');
+        }
+    } else {
+        $scope.submit = function() {
+            $scope.book.bookID = bookList.length + 1;
+            bookList.push($scope.book);
+            $state.go('index');
+        }
+    }
+    $scope.cancel = function() {
+        $state.go('index');
+    }
+
+    function cloneObj(oldObj) { //复制对象方法
+        if (typeof(oldObj) != 'object') return oldObj;
+        if (oldObj == null) return oldObj;
+        var newObj = new Object();
+        for (var i in oldObj)
+            newObj[i] = cloneObj(oldObj[i]);
+        return newObj;
+    }
+});
+
+bookstore.controller('bookDetailCtrl', function($scope, $rootScope, $stateParams, getJson) {
     var bookID = $stateParams.bookID;
     var bookList = [];
     if ($rootScope.books) {
@@ -23,19 +93,21 @@ bookstore.controller('bookDetail', function($scope, $rootScope, $stateParams, ge
         })
     }
 
-});
-
-bookstore.controller('editCtrl', function($scope, $rootScope, $stateParams) {
-    var bookList = $rootScope.books;
-    getBookDetail(bookID, bookList);
-});
-
-
-function getBookDetail(bookID, bookList) {
-    for (var i = 0; i < bookList.length; i++) {
-        if (bookID == bookList[i].bookID) {
-            $scope.book = bookList[i];
-            $scope.bookIntro = bookList[i].intro;
+    function getBookDetail(bookID, bookList) {
+        for (var i = 0; i < bookList.length; i++) {
+            if (bookID == bookList[i].bookID) {
+                $scope.book = bookList[i];
+                $scope.bookIntro = bookList[i].intro;
+            }
         }
     }
-}
+
+});
+
+
+bookstore.controller('mainCtrl', function($scope, $rootScope) {
+    $rootScope.isShow = true;
+    $scope.change = function() {
+        $rootScope.isShow = !$rootScope.isShow;
+    }
+});
